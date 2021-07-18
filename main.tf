829b20b7a1ca9a9b94d5f35a03e6d8e3703f6c09
hemantgadiya <57321739+hemantgadiya@users.noreply.github.com>
2021-07-18 17:41:09 +0530

    Update main.tf
    updated

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
 resource "google_storage_bucket" "bucket" {
-  name = "hsg-bucket-random-112233"
-}
+		name = "hsg-bucket-random-001122"
+	}
+	resource "google_storage_bucket" "gcs_bucket" {
+		name = "hsg-bucket-random-001123"
+	}
