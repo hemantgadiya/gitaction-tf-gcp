f257bad05b96715bd3c35d78f4673cac5bfa06c6
hemantgadiya <57321739+hemantgadiya@users.noreply.github.com>
2021-07-18 17:45:03 +0530

    Update providers.tf
    updated

--- providers.tf
+++ providers.tf
@@ -2,10 +2,3 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-
-terraform {
-  backend "gcs" {
-    bucket = "hsg-tf-state-prod"
-    prefix = "terraform/state"
-  }
-}
